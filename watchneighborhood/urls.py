0484b8deca783312d0fbb5a7a399e04ffb46e9ba
Koech-code <nixon.koech@moringaschool.com>
2021-09-26 12:50:20 +0300

    add the custom view urls and remove them later

--- watchneighborhood/urls.py
+++ watchneighborhood/urls.py
@@ -6,7 +6,12 @@ from . import views
 
 urlpatterns=[
     url('^$', views.index, name='home'),
+    url('all-hoods/', views.neighborhood, name='hood'),
     url('logout/', LogoutView.as_view(), {"next_page":''}),
+    url('join/(\d+)', views.join_hood, name='hoodjoin'),
+    url('leave/(\d+)', views.leave_hood, name='hoodleave'),
+    url('profile/<user>', views.profile, name='profile'),
+
 ]
 
 if settings.DEBUG:
