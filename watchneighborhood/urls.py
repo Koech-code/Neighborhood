cf06e0c270be9f693ab9366e836acde5719812dd
Koech-code <nixon.koech@moringaschool.com>
2021-09-27 01:22:51 +0300

    create the urls for the post, business search and creating a new hood

--- watchneighborhood/urls.py
+++ watchneighborhood/urls.py
@@ -1,17 +1,18 @@
 from django.conf.urls import url
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.auth.views import LogoutView
 from . import views
 
 urlpatterns=[
     url('^$', views.index, name='home'),
     url('all-hoods/', views.neighborhood, name='hood'),
-    url('logout/', LogoutView.as_view(), {"next_page":''}),
     url('join/(\d+)', views.join_hood, name='hoodjoin'),
     url('leave/(\d+)', views.leave_hood, name='hoodleave'),
-    url('profile/<user>', views.profile, name='profile'),
-
+    url('profile/', views.profile, name='profile'),
+    url('create-hoods/', views.create_hood, name='create'),
+    url('new-post/(\d+)', views.create_post, name='post'),
+    url('business/', views.business, name='business'),
+    url('search/', views.business_search, name='search'),
 ]
 
 if settings.DEBUG:
